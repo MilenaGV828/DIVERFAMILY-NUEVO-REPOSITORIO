0fa20138fe3ae76eb76865b3f50e3b383427e5aa
MilenaGV828 <190426183+MilenaGV828@users.noreply.github.com>
2025-05-24 13:22:24 -0500

    depuracion de codigo base de datos

--- Controllers/BASEDEDATOS.js
+++ Controllers/BASEDEDATOS.js
@@ -14,56 +14,9 @@ measurementId: "G-XVKBT2N8XJ"
 };
 
 // Inicializar Firebase y Firestore
-//export const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
 
 
-// Elementos del DOM
-const formulario = document.getElementById("formulario");
-//const listaUsuarios = document.getElementById("lista-usuarios");
-const inputUsuario = document.getElementById("nombre");
-const inputPassword = document.getElementById("contrasena");
-
-let idEdicion = null; // Guardará el ID del usuario en edición
-
-// Función para agregar o actualizar usuario
-const guardarUsuario = async (usuario, contraseña) => {
-  try {
-    
-      await addDoc(collection(db, "USUARIOS"), { usuario, contraseña });
-    
-  } catch (error) {
-    console.error("Error al guardar usuario:", error);
-  }
-};
-
-// Evento para guardar usuario (nuevo o editado)
-formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const usuario = inputUsuario.value;
-  const contraseña = inputPassword.value;
-  guardarUsuario(usuario, contraseña);
-});
-
-
-
-
-///////configuracion original firebase
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-analytics.js";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// export const db = firebase.firestore(); // Exportable si usas módulos
-// // export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 
